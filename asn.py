6bed930fc356ebf1fa2926c15c595de2e63830a0
cpbscholten <c.p.b.scholten@student.utwente.nl>
2020-09-23 17:33:23 +0200

    we've got a list of ASN numbers

--- asn.py
+++ asn.py
@@ -10,10 +10,10 @@ from ipwhois.asn import IPASN
 if __name__ == "__main__":
     base_path = Path().absolute()
     data = pandas.read_csv(base_path / 'csv.txt', header=8, nrows=1000, parse_dates=['dateadded'])
-    ips_urls = data['url'].str.split('/').str[2].str.split(':').str[0]
+    ips_urls = data['url'].str.split('/').str[2].str.split(':').str[0].str.split('www.').str[0]
 
-    ips = [(socket.gethostbyname(x) if bool(re.match(r"^(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})$", x)) else x) for x in ips_urls]
-    asn = [IPASN(Net(x)).lookup()['asn'] for x in ips]
+    ips = [(socket.gethostbyname(x) if not bool(re.match(r"^(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})$", x)) else x) for x in ips_urls]
+    asn = [IPASN(Net(x)).lookup()['asn'] if x != '0.0.0.0' else None for x in ips]
 
 
     print(asn)
