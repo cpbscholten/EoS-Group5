0c31b15277f8edb9d5f0ee062147e134b5651d1e
cpbscholten <c.p.b.scholten@student.utwente.nl>
2020-09-24 11:47:36 +0200

    now added location of hp and average response time

--- asn.py
+++ asn.py
@@ -1,4 +1,3 @@
-#%%
 import pandas
 from pathlib import Path
 import socket
@@ -8,14 +7,26 @@ from ipwhois.asn import IPASN
 
 
 if __name__ == "__main__":
+    # load csv files
     base_path = Path().absolute()
-    data = pandas.read_csv(base_path / 'csv.txt', header=8, nrows=1000, parse_dates=['dateadded'])
+    data = pandas.read_csv(base_path / 'csv.txt', header=8, nrows=100, parse_dates=['dateadded'])
+    response_data = pandas.read_csv(base_path / 'asn_response_data.csv').set_index('ASN')
+    # print(response_data)
+
+    # convert urls to ip addresses
     ips_urls = data['url'].str.split('/').str[2].str.split(':').str[0].str.split('www.').str[0]
-
     ips = [(socket.gethostbyname(x) if not bool(re.match(r"^(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})$", x)) else x) for x in ips_urls]
-    asn = [IPASN(Net(x)).lookup()['asn'] if x != '0.0.0.0' else None for x in ips]
 
+    # retrieve ASN number and location
+    asn_data = [IPASN(Net(x)).lookup() if x != '0.0.0.0' else None for x in ips]
+    asn = [x['asn'] if x else None for x in asn_data]
+    data['asn'] = asn
+    asn_loc = [x['asn_country_code'] if x else None for x in asn_data]
+    data['asn_loc'] = asn_loc
 
-    print(asn)
+    # add average response time
+    data['avg_response_time'] = [response_data.loc['AS' + x, :]['Average Reaction Time'] if x else None for x in asn]
+
+    print(data)
 
 # %%
