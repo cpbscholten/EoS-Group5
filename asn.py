0118f6d5349960a1a4867a7b2ef83a99b82b8cce
cpbscholten <c.p.b.scholten@student.utwente.nl>
2020-09-24 12:35:27 +0200

    added a conversion to seconds

--- asn.py
+++ asn.py
@@ -6,6 +6,23 @@ from ipwhois.net import Net
 from ipwhois.asn import IPASN
 
 
+def string_to_seconds_list(string_list):
+    seconds_list = []
+    for entry in string_list:
+        months = re.findall(r'(\d+) months', entry)
+        days = re.findall(r'(\d+) days', entry)
+        hours = re.findall(r'(\d+) hours', entry)
+        minutes = re.findall(r'(\d+) minutes', entry)
+
+        seconds = 0
+        seconds += int(minutes[0]) * 60 if minutes else 0
+        seconds += int(hours[0]) * 3600 if hours else 0
+        seconds += int(days[0]) * 86400 if days else 0
+        seconds += int(months[0]) * 2628000 if months else 0
+        seconds_list.append(seconds)
+    return seconds_list
+
+
 if __name__ == "__main__":
     # load csv files
     base_path = Path().absolute()
@@ -21,12 +38,16 @@ if __name__ == "__main__":
     asn_data = [IPASN(Net(x)).lookup() if x != '0.0.0.0' else None for x in ips]
     asn = [x['asn'] if x else None for x in asn_data]
     data['asn'] = asn
-    asn_loc = [x['asn_country_code'] if x else None for x in asn_data]
-    data['asn_loc'] = asn_loc
+    data['asn_loc'] = [x['asn_country_code'] if x else None for x in asn_data]
 
     # add average response time
-    data['avg_response_time'] = [response_data.loc['AS' + x, :]['Average Reaction Time'] if x else None for x in asn]
+    avg_response_string = [response_data.loc['AS' + x, :]['Average Reaction Time'] if x else None for x in asn]
+    data['avg_response_time'] = avg_response_string
+    # convert this to seconds for
+    data['avg_response_time_seconds'] = string_to_seconds_list(avg_response_string)
 
     print(data)
 
-# %%
+
+
+
