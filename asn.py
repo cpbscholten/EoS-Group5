451b252b5ae60cb885af69a8728200199f8baa70
cpbscholten <c.p.b.scholten@student.utwente.nl>
2020-09-28 11:13:16 +0200

    added a counter for mapping for the size of the HP and fixed a None/-1 mistake

--- asn.py
+++ asn.py
@@ -19,6 +19,7 @@ def string_to_seconds_list(string_list):
         seconds += int(hours[0]) * 3600 if hours else 0
         seconds += int(days[0]) * 86400 if days else 0
         seconds += int(months[0]) * 2628000 if months else 0
+        seconds = -1 if seconds == 0 else seconds
         seconds_list.append(seconds)
     return seconds_list
 
@@ -28,6 +29,8 @@ if __name__ == "__main__":
     base_path = Path().absolute()
     data = pandas.read_csv(base_path / 'csv.txt', header=8, nrows=100, parse_dates=['dateadded'])
     response_data = pandas.read_csv(base_path / 'asn_response_data.csv').set_index('ASN')
+    domains_hosted_on_asn = pandas.read_csv(base_path / 'domains_hosted_per_asn.csv').set_index('ASN')
+    # print(domains_hosted_on_asn)
     # print(response_data)
 
     # convert urls to ip addresses
@@ -50,6 +53,9 @@ if __name__ == "__main__":
     data['avg_response_time'] = avg_response_string
     # convert this to seconds for
     data['avg_response_time_seconds'] = string_to_seconds_list(avg_response_string)
+
+    # add number of domains hosted on ASN
+    data['domains_hosted'] = [domains_hosted_on_asn.loc['AS' + x, :]['domains'] if 'AS'+ x in domains_hosted_on_asn.index and x else -1 for x in asn]
 
     # Count instances of asn_name
     print(data.asn_name.value_counts())
