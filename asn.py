4ba2a06e34c98f22b488f2bf82882911700e07c9
cpbscholten <c.p.b.scholten@student.utwente.nl>
2020-09-28 12:09:25 +0200

    fixed a few bugs and optimized runtime

--- asn.py
+++ asn.py
@@ -1,3 +1,5 @@
+from typing import Dict
+
 import pandas
 from pathlib import Path
 import socket
@@ -5,22 +7,26 @@ import re
 from ipwhois.net import Net
 from ipwhois.asn import IPASN
 
+domain_to_ip = {}
+ip_to_asn = {}
 
 def string_to_seconds_list(string_list):
     seconds_list = []
     for entry in string_list:
-        months = re.findall(r'(\d+) months', entry)
-        days = re.findall(r'(\d+) days', entry)
-        hours = re.findall(r'(\d+) hours', entry)
-        minutes = re.findall(r'(\d+) minutes', entry)
+        if entry:
+            months = re.findall(r'(\d+) months', entry)
+            days = re.findall(r'(\d+) days', entry)
+            hours = re.findall(r'(\d+) hours', entry)
+            minutes = re.findall(r'(\d+) minutes', entry)
 
-        seconds = 0
-        seconds += int(minutes[0]) * 60 if minutes else 0
-        seconds += int(hours[0]) * 3600 if hours else 0
-        seconds += int(days[0]) * 86400 if days else 0
-        seconds += int(months[0]) * 2628000 if months else 0
-        seconds = -1 if seconds == 0 else seconds
-        seconds_list.append(seconds)
+            seconds = 0
+            seconds += int(minutes[0]) * 60 if minutes else 0
+            seconds += int(hours[0]) * 3600 if hours else 0
+            seconds += int(days[0]) * 86400 if days else 0
+            seconds += int(months[0]) * 2628000 if months else 0
+            seconds_list.append(seconds)
+        else:
+            seconds_list.append(-1)
     return seconds_list
 
 
@@ -31,10 +37,28 @@ def get_ip(hostname):
         return '0.0.0.0'
 
 
+def ip_to_asn_lookup(ip: str) -> str:
+    if ip in ip_to_asn:
+        return ip_to_asn[ip]
+    else:
+        asn_info = IPASN(Net(ip)).lookup()
+        ip_to_asn[ip] = asn_info
+        return asn_info
+
+
+def url_to_ip_lookup(url: str) -> str:
+    if url in domain_to_ip:
+        return domain_to_ip[url]
+    else:
+        ip_no = get_ip(url)
+        domain_to_ip[url] = ip_no
+        return ip_no
+
+
 if __name__ == "__main__":
     # load csv files
     base_path = Path().absolute()
-    data = pandas.read_csv(base_path / 'csv.txt', header=8, parse_dates=['dateadded'])
+    data = pandas.read_csv(base_path / 'csv.txt', header=8,  nrows=800, parse_dates=['dateadded'])
     response_data = pandas.read_csv(base_path / 'asn_response_data.csv').set_index('ASN')
     domains_hosted_on_asn = pandas.read_csv(base_path / 'domains_hosted_per_asn.csv').set_index('ASN')
     # print(domains_hosted_on_asn)
@@ -42,17 +66,20 @@ if __name__ == "__main__":
 
     # convert urls to ip addresses
     ips_urls = data['url'].str.split('/').str[2].str.split(':').str[0].str.split('www.').str[0]
-    ips = [(get_ip(x) if not bool(re.match(r"^(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})$", x)) else x) for x in ips_urls]
+    ips = [(url_to_ip_lookup(x) if not bool(re.match(r"^(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})$", x)) else x) for x in ips_urls]
 
     # retrieve ASN number and location
-    asn_data = [IPASN(Net(x)).lookup() if x != '0.0.0.0' else None for x in ips]
-    asn = [x['asn'] if x else None for x in asn_data]
+    asn_data = [ip_to_asn_lookup(x) if x != '0.0.0.0' else None for x in ips]
+    # do a split, since there might be a possibility that multiple ASNs are returned.
+    asn = [x['asn'].split(" ")[0] if x else None for x in asn_data]
+    # print(asn.index("7296"))
 
     data['asn'] = asn
     data['asn_loc'] = [x['asn_country_code'] if x else None for x in asn_data]
 
     # add asn maintainer name
     asn_name = [response_data.loc['AS' + x, :]['Name'] if x else None for x in asn]
+
     data['asn_name'] = asn_name
 
     # add average response time
@@ -62,7 +89,7 @@ if __name__ == "__main__":
     data['avg_response_time_seconds'] = string_to_seconds_list(avg_response_string)
 
     # add number of domains hosted on ASN
-    data['domains_hosted'] = [domains_hosted_on_asn.loc['AS' + x, :]['domains'] if 'AS'+ x in domains_hosted_on_asn.index and x else -1 for x in asn]
+    data['domains_hosted'] = [domains_hosted_on_asn.loc['AS' + x, :]['domains'] if x and 'AS' + x in domains_hosted_on_asn.index else -1 for x in asn]
 
     # Count instances of asn_name
     print(data.asn_name.value_counts())
