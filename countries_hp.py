0033f866d652c5a2450f36069ffc97315f1e9991
cpbscholten <c.p.b.scholten@student.utwente.nl>
2020-09-28 19:42:20 +0200

    added polt of countries

--- countries_hp.py
+++ countries_hp.py
@@ -14,21 +14,23 @@ with open('dataset.csv') as csv_file:
             first_line = False
             pass
         else:
-            country = row[9]
+            country = row[10]
             if country in countries:
                 countries[country] += 1
             else:
                 countries[country] = 1
 
 print(countries)
-ordered_countries = {k: v for k, v in sorted(countries.items(), key=lambda item: item[1])}
+ordered_countries = {k: v for k, v in sorted(countries.items(), key=lambda item: item[1], reverse=True)}
 print(ordered_countries)
 
-plt.bar(ordered_countries.keys(), ordered_countries.values(), width = 0.5, color='#0504aa',alpha=0.7)
+plt.bar(list(ordered_countries.keys())[1:11], list(ordered_countries.values())[1:11], width = 0.5, color='#0504aa',alpha=0.7)
 plt.grid(axis='y', alpha=0.75)
 plt.xlabel('Country',fontsize=15)
 plt.ylabel('Istances',fontsize=15)
 plt.xticks(fontsize=10)
 plt.yticks(fontsize=10)
 plt.title('Distribution of HPs',fontsize=15)
+
+plt.savefig("country.pdf")
 plt.show()
--- test.py
+++
@@ -1,8 +0,0 @@
-# %%
-from ipwhois import IPWhois
-import dns.resolver
-
-if __name__ == "__main__":
-    dns_name = dns.resolver.query('dnspython.org', 'A').response.answer[0]
-    test = IPWhois('27.5.16.43').lookup_whois()
-    print(test)
