a906394a66d8161a04c041a9fd0b5df142b0cd8a
MegaCookie <mail@jessevandervelden.nl>
2020-10-05 15:39:30 +0200

    Strip country names

--- speedtest-countries.py
+++ speedtest-countries.py
@@ -1,9 +1,12 @@
+#%%
 import pandas
 
-data_countries_instances = pandas.read_csv("results/countries_list.csv", usecols=['country', 'count']).set_index('country')
-data_countries_speed = pandas.read_csv("Data/speedtest.csv", usecols=['country', 'speed']).set_index('country')
-print(data_countries_speed)
+data_countries_instances = pandas.read_csv("results/countries_list.csv", usecols=['country', 'count'])
+data_countries_instances['country'] = data_countries_instances['country'].str.strip()
+
+data_countries_speed = pandas.read_csv("Data/speedtest.csv", usecols=['country', 'speed'])
+data_countries_speed['country'] = data_countries_speed['country'].str.strip()
 
 
-both = pandas.merge(data_countries_instances, data_countries_speed, how='left', left_index=True, right_index=True)
+both = pandas.merge(data_countries_instances, data_countries_speed, how='left', left_on='country', right_on='country')
 print(both.corr())
