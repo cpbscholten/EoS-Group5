d810fce7707ed87a364053225ffd33129eab484f
cpbscholten <c.p.b.scholten@student.utwente.nl>
2020-10-05 15:16:06 +0200

    speedtest

--- speedtest-countries.py
+++ speedtest-countries.py
@@ -1,6 +1,9 @@
 import pandas
 
-data_countries_instances = pandas.read_csv("results/countries_list.csv")
-data_countries_speed = pandas.read_csv("Data/speedtest.csv")
+data_countries_instances = pandas.read_csv("results/countries_list.csv", usecols=['country', 'count']).set_index('country')
+data_countries_speed = pandas.read_csv("Data/speedtest.csv", usecols=['country', 'speed']).set_index('country')
+print(data_countries_speed)
 
 
+both = pandas.merge(data_countries_instances, data_countries_speed, how='left', left_index=True, right_index=True)
+print(both.corr())
